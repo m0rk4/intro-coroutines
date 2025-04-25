fd990a76953771da15c18f5ac3f117bfad963e8a
Mark Putsiata <m.putsiata@gmail.com>
2025-04-26 00:05:51 +0300

    finish task 5

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -1,8 +1,29 @@
 package tasks
 
-import contributors.*
-import kotlinx.coroutines.*
+import contributors.GitHubService
+import contributors.RequestData
+import contributors.User
+import contributors.log
+import contributors.logRepos
+import contributors.logUsers
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service.getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    repos.map { repo ->
+        async(Dispatchers.Default) {
+            log("starting loading for ${repo.name}")
+            service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }.awaitAll()
+        .flatten()
+        .aggregate()
 }
