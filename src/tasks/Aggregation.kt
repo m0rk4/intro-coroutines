14ea48e821fec0f4396fe9a84b4f06fbe5cf0276
Mark Putsiata <m.putsiata@gmail.com>
2025-04-25 22:57:18 +0300

    finish task 1

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -3,16 +3,13 @@ package tasks
 import contributors.User
 
 /*
-TODO: Write aggregation code.
-
  In the initial list each user is present several times, once for each
  repository he or she contributed to.
  Merge duplications: each user should be present only once in the resulting list
  with the total value of contributions for all the repositories.
- Users should be sorted in a descending order by their contributions.
-
- The corresponding test can be found in test/tasks/AggregationKtTest.kt.
- You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
+ Users should be sorted in descending order by their contributions.
 */
 fun List<User>.aggregate(): List<User> =
-    this
+    this.groupBy({ it.login }) { it.contributions }
+        .map { (login, contributions) -> User(login, contributions.sum()) }
+        .sortedByDescending { it.contributions }
